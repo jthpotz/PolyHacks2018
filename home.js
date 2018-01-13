5117a4fb353c278baa431a72f90ee098a19d2333
jthpotz <jthpotz@gmail.com>
2018-01-13 11:28:56 -0800

    sticky nav sticky again

--- home.js
+++ home.js
@@ -1,14 +1,17 @@
 // When the user scrolls the page, execute myFunction
+var navbar;
+var locationbar;
+var stickyNav;
 window.onscroll = function() {stick()};
+window.onload = function() {
+    // Get the navbar
+    navbar = document.getElementById("navbar");
+    console.log(navbar);
+    locationbar = document.getElementById("locationbar");
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
-console.log(navbar);
-var locationbar = document.getElementById("locationbar");
-
-// Get the offset position of the navbar
-var stickyNav = navbar.offsetTop;
-
+    // Get the offset position of the navbar
+    stickyNav = navbar.offsetTop;
+}
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stick() {
