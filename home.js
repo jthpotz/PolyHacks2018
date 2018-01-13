49d9ea1678058548baec7b5f9b1a9a91b8850103
jthpotz <jthpotz@gmail.com>
2018-01-13 14:49:39 -0800

    room and category navigation

--- home.js
+++ home.js
@@ -4,7 +4,14 @@ var locationbar;
 var stickyNav;
 var stickyLoc;
 var location_pos = 0;
-var room_names = ["green_square", "green_square", "green_square", "green_square", "green_square", "green_square", "green_square", "green_square", "green_square", "green_square", "green_square", "green_square"];
+var category_pos = 0;
+var room_text;
+var room_names = ["Master Bathroom", "Bathroom", "Master Bedroom", "Kid\'s Bedroom", "Living Room", "Kitchen"];
+var category_names = ["Furniture", "Electronics", "Personal Items", "Office Supplies", "Plants"];
+var category_text;
+var room_box;
+var cat_box;
+var cur_choice = "Room";
 window.onscroll = function() {stick()};
 window.onload = function() {
     // Get the navbar
@@ -17,7 +24,14 @@ window.onload = function() {
     stickyLoc = locationbar.offsetTop;
     
     load_locations();
+    load_category();
+    room_text = document.getElementById("room");
+    category_text = document.getElementById("category");
+    room_box = document.getElementById("room_list");
+    cat_box = document.getElementById("category_list");
 }
+
+
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stick() {
@@ -63,13 +77,14 @@ function load_locations(){
     var top_row = true;
     
     
-    while(i < room_names.length && i < 12){
+    while(i < room_names.length && i < 8){
         var elem = document.createElement("img");
-        console.log(room_names[i] + ".png");
-        elem.setAttribute("src", room_names[i] + ".png");
+        console.log("icons/rooms/" + room_names[i] + ".png");
+        elem.setAttribute("src", "icons/rooms/" + room_names[i] + ".jpg");
         elem.setAttribute("class", "location_icon");
+        elem.setAttribute("onclick", "set_room(\"" + room_names[i] + "\")");
         var div;
-        if(i < 6){
+        if(i < 4){
             div = document.getElementById("top_loc");
         }
         else{
@@ -78,5 +93,47 @@ function load_locations(){
         div.appendChild(elem);
         i++;
     }
-    pos = i;
+    location_pos = i;
+}
+
+function load_category(){
+    var i = 0;
+    var top_row = true;
+    
+    
+    while(i < category_names.length && i < 6){
+        var elem = document.createElement("img");
+        console.log("icons/tags/" + category_names[i] + ".png");
+        elem.setAttribute("src", "icons/tags/" + category_names[i] + ".jpg");
+        elem.setAttribute("class", "location_icon");
+        elem.setAttribute("onclick", "set_category(\"" + category_names[i] + "\")");
+        var div;
+        if(i < 3){
+            div = document.getElementById("top_cat");
+        }
+        else{
+            div = document.getElementById("bot_cat");
+        }
+        div.appendChild(elem);
+        i++;
+    }
+    category_pos = i;
+}
+
+function set_room(name){
+    room_text.innerHTML = "Room: " + name;
+    room_box.style.display = "none";
+    cat_box.style.display = "flex";
+}
+
+function set_category(category){
+    category_text.innerHTML = "Category: " + category;
+    cat_box.style.display = "none";
+}
+
+function reset(){
+    room_text.innerHTML = "Room: None Selected";
+    category_text.innerHTML = "Category: None Selected";
+    room_box.style.display = "flex";
+    cat_box.style.display = "none";
 }
