10859a39faa88553638d1d67297af9e9e05251ed
jthpotz <jthpotz@gmail.com>
2018-01-13 13:53:59 -0800

    icon loading and positioning

--- home.js
+++ home.js
@@ -3,6 +3,8 @@ var navbar;
 var locationbar;
 var stickyNav;
 var stickyLoc;
+var location_pos = 0;
+var room_names = ["green_square", "green_square", "green_square", "green_square", "green_square", "green_square", "green_square", "green_square", "green_square", "green_square", "green_square", "green_square"];
 window.onscroll = function() {stick()};
 window.onload = function() {
     // Get the navbar
@@ -13,6 +15,8 @@ window.onload = function() {
     // Get the offset position of the navbar
     stickyNav = navbar.offsetTop;
     stickyLoc = locationbar.offsetTop;
+    
+    load_locations();
 }
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
@@ -52,4 +56,27 @@ function show_flex(id) {
 function change_view(show_id, hide_id){
     hide(hide_id);
     show(show_id);
+}
+
+function load_locations(){
+    var i = 0;
+    var top_row = true;
+    
+    
+    while(i < room_names.length && i < 12){
+        var elem = document.createElement("img");
+        console.log(room_names[i] + ".png");
+        elem.setAttribute("src", room_names[i] + ".png");
+        elem.setAttribute("class", "location_icon");
+        var div;
+        if(i < 6){
+            div = document.getElementById("top_loc");
+        }
+        else{
+            div = document.getElementById("bot_loc");
+        }
+        div.appendChild(elem);
+        i++;
+    }
+    pos = i;
 }
