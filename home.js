2d12431c018d4b38f12b1ac0ccd93c2c9535cbee
jthpotz <jthpotz@gmail.com>
2018-01-13 11:05:33 -0800

    started javascript for showing/hiding divs; sticky broken

--- home.js
+++ home.js
@@ -1,17 +1,50 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {stick()};
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
+console.log(navbar);
+var locationbar = document.getElementById("locationbar");
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+var stickyNav = navbar.offsetTop;
+
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+function stick() {
+  if (window.pageYOffset >= stickyNav) {
+    navbar.classList.add("sticky");
+    Console.log("func stuck");
   } else {
     navbar.classList.remove("sticky");
   }
-} 
+  if (window.pageYOffset >= stickyNav) {
+    navbar.classList.add("sticky");
+    Console.log("func stuck");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+function hide(id) {
+    if(typeof(id) == "string"){
+        document.getElementById(id).style.display = none;
+    }
+    else{
+        console.log("Not a proper id");
+    }
+}
+
+function show_flex(id) {
+    if(typeof(id) == "string"){
+        document.getElementById(id).style.display = flex;
+    }
+    else{
+        console.log("Not a proper id");
+    }
+}
+
+function change_view(show_id, hide_id){
+    hide(hide_id);
+    show(show_id);
+}
